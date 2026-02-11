6371f061a5e32c5ae33fe302dee7de13f0c607d6
Dongyeop Kang <dykang85@gmail.com>
2026-02-11 13:34:25 -0600

    update

--- static/js/index.js
+++ static/js/index.js
@@ -4,17 +4,28 @@ window.HELP_IMPROVE_VIDEOJS = false;
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+    // Initialize Slick carousel for Research Highlights
+    $('#results-carousel').slick({
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    dots: true
+                }
+            }
+        ]
+    });
 	
     bulmaSlider.attach();
 
